8d4995c53f5f8c88d48b6dd6683a15ac71bfa7df
Ander <ander112405@gmail.com>
2026-07-07 14:23:08 -0600

    Mas atributos

--- Documents/NetBeansProjects/TF_GrupoN4/src/main/java/model/Medicamento.java
+++ Documents/NetBeansProjects/TF_GrupoN4/src/main/java/model/Medicamento.java
@@ -3,10 +3,110 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package model;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 /**
  *
- * @author Anderson
+ * @author Grupo Farmacia
  */
+//Clase que representa un medicamento de la farmacia.
 public class Medicamento {
+    //Atributos privados del medicamento (Encapsulamiento)
+    private String codigo;
     private String nombre;
+    private String laboratorio;
+    private int stock;                   
+    private double precio;
+    private LocalDate fechaDeVencimiento;
+    private String categoria;
+    private boolean requiereReceta;
+    
+    //Constructor vacio (Sobrecarga)
+    public Medicamento() {
+    }
+    //Constructor con 2 atributos
+
+    public Medicamento(String codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
+    //Constructor con todos los atributos
+
+    public Medicamento(String codigo, String nombre, String laboratorio, int stock, double precio, LocalDate fechaDeVencimiento, String categoria, boolean requiereReceta) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.laboratorio = laboratorio;
+        this.stock = stock;
+        this.precio = precio;
+        this.fechaDeVencimiento = fechaDeVencimiento;
+        this.categoria = categoria;
+        this.requiereReceta = requiereReceta;
+    }
+    
+    //Getters y Setters de los 
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLaboratorio() {
+        return laboratorio;
+    }
+
+    public void setLaboratorio(String laboratorio) {
+        this.laboratorio = laboratorio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public LocalDate getFechaDeVencimiento() {
+        return fechaDeVencimiento;
+    }
+
+    public void setFechaDeVencimiento(LocalDate fechaDeVencimiento) {
+        this.fechaDeVencimiento = fechaDeVencimiento;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public boolean isRequiereReceta() {
+        return requiereReceta;
+    }
+
+    public void setRequiereReceta(boolean requiereReceta) {
+        this.requiereReceta = requiereReceta;
+    }
+    
+    
 }
