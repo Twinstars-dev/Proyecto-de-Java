d885d7d203b79779c83de942ec10b85845a3dc5e
Pablito1234 <geovanniuretarojas@gmail.com>
2026-07-10 13:36:15 -0500

    push de geovanni

--- Documents/NetBeansProjects/TF_GrupoN4/src/main/java/dao/GestionVentas.java
+++ Documents/NetBeansProjects/TF_GrupoN4/src/main/java/dao/GestionVentas.java
@@ -4,10 +4,84 @@
  */
 package dao;
 
+import java.time.LocalDate;
+import model.Venta;
+import model.Cliente;
 /**
  *
  * @author Anderson
  */
-public class GestionVentas {
-    
+public class GestionVentas{   
+private Venta[] ventas;
+private int contador;
+
+public GestionVentas() {
+    this.ventas = new Venta[50];
+    this.contador = 0;
+}
+
+public void listarVentas(){
+    StringBuilder sb = new StringBuilder();
+    Cliente cl = new Cliente();
+        for(int i=0;i<contador;i++){
+        sb.append("Venta N°: ")
+          .append(ventas[i].getId())
+          .append("\n");
+        sb.append("Cliente: ")
+          .append(cl.getNombre())
+          .append("\n");
+        sb.append("Monto: ")
+          .append(ventas[i].getPreciodeventa())
+          .append("\n\n");
+    }
+
+    System.out.println(sb.toString());
+    System.out.println("Cantidad de ventas realizadas: "+ contador);
+}
+
+public void buscarVenta(int codigo){
+    for(int i=0; i<contador;i++){  
+       if(ventas[i]!=null && ventas[i].getId()==codigo){
+           System.out.println("Venta encontrada");    
+           System.out.println("Fecha del medicamento vendido: "+ventas[i].getFechavendida());    
+           System.out.println("Nombre del medicamento: "+ ventas[i].getNombredelmedicamento());    
+           System.out.println("Cantidad comprada: "+ventas[i].getCantidadeproductos());    
+           System.out.println("Monto total pagado: "+ventas[i].getPreciodeventa());    
+       } 
+   } 
+}
+
+public double calcularIngresosTotales(){
+  double ingresototal=0.0;
+  for(int i=0; i<contador;i++){
+      if(ventas[i]!=null){
+         ingresototal+=ventas[i].Costotal(); 
+      }
+   }
+    return ingresototal;
+ } 
+
+public void fechaMayorIngreso() {
+    if (contador == 0){
+        System.out.println("No hay ventas registradas.");
+        return;
+    }
+    LocalDate fechaMayor = ventas[0].getFechavendida();
+    double mayorIngreso = 0;
+    for (int i = 0; i < contador; i++){
+        double suma = 0;
+        for (int j = 0; j < contador; j++) {
+             if (ventas[i].getFechavendida().equals(ventas[j].getFechavendida())) {
+                 suma += ventas[j].getPreciodeventa();
+            }
+         }
+             if (suma > mayorIngreso) {
+                 mayorIngreso = suma;
+                 fechaMayor = ventas[i].getFechavendida();
+            }
+         }
+    System.out.println("Fecha con mayor ingreso: " + fechaMayor);
+    System.out.println("Ingreso total: S/. " + mayorIngreso);
+}
+
 }
